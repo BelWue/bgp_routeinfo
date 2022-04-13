1f2f63da2cd8f9858d3a6f7c5525b072b1c69907
Sebastian Neuner <neuner@belwue.de>
2022-04-13 13:38:18 +0200

    repaired some benchmarks

--- routeinfo/routeinfo_test.go
+++ routeinfo/routeinfo_test.go
@@ -3,17 +3,17 @@ package routeinfo
 import (
 	"io/ioutil"
 	"log"
+	"math/rand"
+	"strconv"
 	"testing"
-    //"math/rand"
-    //"strconv"
-    "time"
+	"time"
 
 	"gopkg.in/yaml.v2"
 )
 
 var rs RouteInfoServer
 var router *Router
-const routerName = "fra-decix-1"
+
 const addressCacheSize = 10000
 
 func init() {
@@ -26,52 +26,62 @@ func init() {
 	if err != nil {
 		log.Fatalf("[error] Error parsing configuration YAML: %v", err)
 	}
-    rs.Init()
-    router = rs.Routers[routerName]
+	rs.Init()
+	for _, value := range rs.Routers {
+		// simply use whatever the first router is
+		router = value
+		break
+	}
 
-    // wait for sessions to be established and in sync
-    time.Sleep(10 * time.Second)
-    for {
-        _, ready := router.Status()
-        if ready == true {
-            break
-        } else {
-            time.Sleep(3 * time.Second)
-        }
-    }
-    // for good measure
-    time.Sleep(10 * time.Second)
+	// wait for sessions to be established and in sync
+	time.Sleep(10 * time.Second)
+	for {
+		_, ready := router.Status()
+		if ready == true {
+			break
+		} else {
+			time.Sleep(3 * time.Second)
+		}
+	}
+	// for good measure
+	time.Sleep(10 * time.Second)
 }
 
-//func BenchmarkLookupIPv4Random(b *testing.B) {
-//    addresses := make([]string, addressCacheSize)
-//    for n := 0; n < addressCacheSize; n++ {
-//        addresses[n] = strconv.Itoa(rand.Intn(255)) + "." + strconv.Itoa(rand.Intn(255)) + "." + strconv.Itoa(rand.Intn(255)) + "." + strconv.Itoa(rand.Intn(255))
-//    }
-//
-//    // let's goooo
-//    b.ResetTimer()
-//	for n := 0; n < b.N; n++ {
-//		router.Lookup(addresses[n % addressCacheSize])
-//	}
-//}
-//
-//func BenchmarkLookupIPv4Static(b *testing.B) {
-//    addresses := make([]string, addressCacheSize)
-//    for n := 0; n < addressCacheSize; n++ {
-//        addresses[n] = "1.1.1.1"
-//    }
-//
-//    // let's goooo
-//    b.ResetTimer()
-//	for n := 0; n < b.N; n++ {
-//		router.Lookup(addresses[n % addressCacheSize])
-//	}
-//}
+func BenchmarkLookupIPv4Random(b *testing.B) {
+	addresses := make([]string, addressCacheSize)
+	for n := 0; n < addressCacheSize; n++ {
+		addresses[n] = strconv.Itoa(rand.Intn(255)) + "." + strconv.Itoa(rand.Intn(255)) + "." + strconv.Itoa(rand.Intn(255)) + "." + strconv.Itoa(rand.Intn(255))
+	}
+
+	// let's goooo
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		router.Lookup(addresses[n%addressCacheSize])
+	}
+}
+
+func BenchmarkLookupIPv4Static(b *testing.B) {
+	addresses := make([]string, addressCacheSize)
+	for n := 0; n < addressCacheSize; n++ {
+		addresses[n] = "1.1.1.1"
+	}
+
+	// let's goooo
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		router.Lookup(addresses[n%addressCacheSize])
+	}
+}
+
+func BenchmarkEstablished(b *testing.B) {
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		router.Established()
+	}
+}
 
 func BenchmarkStatus(b *testing.B) {
-    // let's goooo
-    b.ResetTimer()
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		router.Status()
 	}
